d993853ca727eaadd3d3eb4eb72633e04bc15d38
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2023-12-19 12:54:56 +0000

    optimized bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * Description: the biggest elements are sorted before the big ones
+ * using bubble sort algorithm
+ *
+ * @array: array of integers to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t check;
+	size_t j;
+	size_t new;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			check = 0;
+			if (array[j] > array[j + 1])
+			{
+				new = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = new;
+				print_array(array, size);
+				check = 1;
+			}
+		}
+		if (check == 0)
+			return;
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This repository looks at the concepts of sorting algorithms and big-Oh notation
+in C. Many algorithms will be implemented and their omega, Oh and theta values
+discussed.
